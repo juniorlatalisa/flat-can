e2e2fec2ab8a9a75eb09d428a657e0a09663fd0b
juniorlatalisa <juniorlatalisa@Acer-Helios300.local>
2020-10-22 19:50:35 -0300

    ObjectUtils

---
+++ common-utils/src/test/java/br/com/virtualsistemas/common/utils/ObjectUtilsTest.java
@@ -0,0 +1,18 @@
+package br.com.virtualsistemas.common.utils;
+
+import java.time.LocalDateTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ObjectUtilsTest {
+
+	@Test
+	public void encodedecode() {
+		LocalDateTime agora = LocalDateTime.now();
+		byte[] buffer = ObjectUtils.encodeObject(agora);
+		LocalDateTime value = ObjectUtils.decodeObject(buffer);
+		Assert.assertEquals(agora, value);
+	}
+
+}
