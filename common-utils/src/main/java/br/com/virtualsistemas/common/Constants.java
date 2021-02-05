7fe05b41580916fb64bb5c6e888ce765757fcbe9
juniorlatalisa <paraojuniorler@gmail.com>
2021-02-05 19:36:49 -0300

    PASSWORD_PATTERN

--- common-utils/src/main/java/br/com/virtualsistemas/common/Constants.java
+++ common-utils/src/main/java/br/com/virtualsistemas/common/Constants.java
@@ -21,21 +21,21 @@ public interface Constants {
 	TimeZone TIME_ZONE = TimeZone.getTimeZone("America/Sao_Paulo");
 
 	String DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm:ss";
-	
+
 	String DATE_PATTERN = "dd/MM/yyyy";
-	
+
 	String TIME_PATTERN = "HH:mm:ss";
 
 	DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN, BRAZIL);
-	
+
 	DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN, BRAZIL);
-	
+
 	DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_PATTERN, BRAZIL);
 
 	SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat(DATE_TIME_PATTERN, BRAZIL);
-	
+
 	SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(DATE_PATTERN, BRAZIL);
-	
+
 	SimpleDateFormat TIME_FORMAT = new SimpleDateFormat(TIME_PATTERN, BRAZIL);
 
 	DecimalFormatSymbols DECIMAL_FORMAT_SYMBOLS = DecimalFormatSymbols.getInstance(BRAZIL);
@@ -68,4 +68,20 @@ public interface Constants {
 	String SHA256_ALGORITHM = "SHA-256";
 
 	String MD5_ALGORITHM = "MD5";
+
+	/**
+	 * For our passwords we are going to implement a strict policy about their
+	 * format. We want our passwords to :
+	 * 
+	 * <li>Be between 8 and 40 characters long
+	 * <li>Contain at least one digit.
+	 * <li>Contain at least one lower case character.
+	 * <li>Contain at least one upper case character.
+	 * <li>Contain at least on special character from [ @ # $ % ! . ].
+	 *
+	 * @see <a href=
+	 *      "https://examples.javacodegeeks.com/core-java/util/regex/matcher/validate-password-with-java-regular-expression-example">Validate
+	 *      Password with Java Regular Expression example</a>
+	 */
+	String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!]).{8,40})";
 }
